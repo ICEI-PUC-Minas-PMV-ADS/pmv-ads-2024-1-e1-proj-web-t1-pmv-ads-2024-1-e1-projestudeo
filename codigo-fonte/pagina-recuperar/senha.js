39b11df3d19bc326526a9761bb7ac8b39d575841
Carolina Félix <163934187+c4rolfelix@users.noreply.github.com>
2024-04-24 21:37:10 -0300

    Update recuperar senha

---
+++ codigo-fonte/pagina-recuperar/senha.js
@@ -0,0 +1,15 @@
+function myFunction(){
+    var x = document.getElementById("password");
+    var y = document.getElementById("showPassword");
+    var z = document.getElementById("hidePassword");
+    if(x.type === 'password'){
+        x.type = "text";
+        y.style.display = "block";
+        z.style.display = "none";
+    }
+    else{
+        x.type = "password";
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+}

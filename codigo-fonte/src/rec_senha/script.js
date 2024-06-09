0354c7332e24c69bf12deaef6ac4b03caf00d66f
Alvaro <alvaro.kumaira@gmail.com>
2024-06-09 07:48:13 -0300

    lógica básica  da página de recuperar senha

--- codigo-fonte/src/rec_senha/script.js
+++ codigo-fonte/src/rec_senha/script.js
@@ -35,3 +35,27 @@ function togglePasswordConfirmation(){
         z.style.display = "block";
     }
 }
+
+document.getElementById('formRec').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    // Captura dos valores dos campos de login
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+    const confsenha = document.getElementById('confirmarSenha').value;
+
+    // Recupera os dados do usuário armazenado no Local Storage
+    const usuario = JSON.parse(localStorage.getItem('usuario'));
+
+    // Verifica se o usuário existe e se as credenciais são válidas
+    if (usuario && usuario.email === email && senha === confsenha) {
+        alert('Senha alterada com sucesso!');
+        // Atualiza a senha do usuário no Local Storage
+        usuario.senha = senha;
+        localStorage.setItem('usuario', JSON.stringify(usuario));
+        // Redireciona para a página de login
+        window.location.href = '../index.html';
+    } else {
+        alert('E-mail não existe ou senhas diferentes. Por favor, tente novamente.');
+    }
+});
